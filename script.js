170936021cf6f814171882a10c6f97e2fd57c140
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-02-09 15:29:31 -0500

    wrote operator functions

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // TOP caluclator script
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if (num2 === 0) {
+        return "LOL!";
+    } else {
+        return num1 / num2;
+    }
+}
+
+function percent(num) {
+    return num / 100;
+}
+
+function plusNegative(num) {
+    return num - (num * 2);
+}
