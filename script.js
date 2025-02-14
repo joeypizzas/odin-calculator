56ae48b24eea7b65903f0e1bde4d0f06ecf33db6
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-02-14 08:44:11 -0500

    added funny message when divide by 0

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     if (num2 === 0) {
-        return "LOL!";
+        return "CHUMP";
     } else {
         return num1 / num2;
     }
